f6a9f5a0eff79a2be536eeddb87c056ebfa91a71
SidharthBsunil <sidharthbsunil@gmail.com>
2026-06-09 10:50:04 +0530

    second commit

--- primefact.c
+++ primefact.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-void main()
+int main()
 {
     int num,arr[4]={2,3,5,7};
     printf("enter a number \n");
@@ -9,8 +9,14 @@ void main()
         while(num%arr[i]==0)
         {
             num=num/arr[i];
-            printf("%d",arr[i]);
+            //printf("%d\t\n",num);
+            printf(" %d,",arr[i]);
 
         }
     }
+    if(num > 1)
+        printf("%d", num);
+
+    
+return 0;
 }
