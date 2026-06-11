66ae5af844aeac2656548fc24a774cafe17d2b7a
SidharthBsunil <sidharthbsunil@gmail.com>
2026-06-11 12:12:54 +0530

    fibo

--- fibo.c
+++ fibo.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void main()
 {
-    int num=0,sum=1,res=0;
+    int num=0,sum=1,res=0,next=0;
 printf("enter a the number to find fibonicci ");
 scanf("%d",&num);
 if(num<1)
@@ -10,12 +10,13 @@ printf("1");
 }
 else
 {
-    printf("1");
+    printf("1\t");
     for(int i=0;i<num;i++)
     {
-        sum+=res;
+        next=sum+res;
         res=sum;
-        printf("%d\t",sum);
+        sum=next;
+        printf("%d\t",next);
     }
 }
 
