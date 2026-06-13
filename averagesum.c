57847222ecbec7f44e3441b3fac23b14f3eabda7
SidharthBsunil <sidharthbsunil@gmail.com>
2026-06-13 16:33:31 +0530

    sum avg

---
+++ averagesum.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+
+void main()
+{
+    int A[20],n,Sum=0;
+    printf("enter the nth number");
+    scanf("%d",&n);
+    int temp=n+1;
+    printf("enter array number");
+    for(int i=0;i<=n;i++)
+    {
+        scanf("%d",&A[i]);
+        Sum+=A[i];
+    }
+    printf("printing ...........\n");
+    printf("Sum is %d\n",Sum);
+    printf("Average is %d",Sum/(temp));
+   
+}
