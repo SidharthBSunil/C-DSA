d35aebf337b220fd64cfbc0cda0406ab26f2b32b
SidharthBsunil <sidharthbsunil@gmail.com>
2026-06-22 22:16:02 +0530

    bug is here tommaorrow check it

---
+++ dynamic memory/msecond.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int *ptr=(int *)malloc(sizeof(int)*5);
+    int i=10;
+    ptr=1;
+    if(ptr==NULL)
+    {
+        printf("memory allocation failed");
+        exit(0);
+    }
+
+    //pr
+
+    printf("number is %d",(*ptr+1));
+    //scanf()
+
+}
