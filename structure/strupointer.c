e980f245c973112b1dc8c7a12ff52c6237f88457
SidharthBsunil <sidharthbsunil@gmail.com>
2026-06-26 17:27:36 +0530

    struct file sharing

---
+++ structure/strupointer.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+struct student
+{
+char name[20];
+int age;
+int rollno;
+};
+
+void main()
+{
+    struct student s1;
+    struct student s2;
+    printf("enter the student name");
+    scanf("%d",&s1.name);
+    printf()
+
+
+}
