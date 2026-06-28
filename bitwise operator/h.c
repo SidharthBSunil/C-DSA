f6a48ec37c0bbcfa0be651aa04d3a20cd9470290
SidharthBsunil <sidharthbsunil@gmail.com>
2026-06-28 21:34:05 +0530

    b

--- bitwise operator/h.c
+++ bitwise operator/h.c
@@ -1,1 +1,28 @@
-#
+#include <stdio.h>
+
+void displayBits(int x);
+
+int main(void)
+{
+    int a = 7;
+
+    printf("a = %d\t", a);
+    displayBits(a);
+
+    return 0;
+}
+
+void displayBits(int x)
+{
+    int i;
+
+    for (i = 31; i >= 0; i--)
+    {
+        printf("%d", (x >> i) & 1);
+
+        if (i % 4 == 0)
+            printf(" ");
+    }
+
+    printf("\n");
+}
