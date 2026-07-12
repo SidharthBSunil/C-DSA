eb35b1955133c7765e0d8ec576751d9e7dc863aa
SidharthBsunil <sidharthbsunil@gmail.com>
2026-07-12 19:34:36 +0530

    pointer function with different value

--- pointers file/pointerfunction.c
+++ pointers file/pointerfunction.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
-int add(int a,int b)
+int add(int a, int b)
 {
     return a+b;
 }
 int main()
 {
-int (*ptr)(int,int);
-ptr=add;
-printf("number is %d",ptr(5,6));
+    int (*ptr)(int,int);
+    ptr=add;
+    printf("result is %d",ptr(10,12));
 }
